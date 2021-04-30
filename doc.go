6186c4ebd14fc7d01e24289e5af1f49a26599962
Koichi Shiraishi <zchee.io@gmail.com>
2021-04-30 22:54:02 +0900

    doc: add package top-level synopsis

---
+++ doc.go
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: Copyright 2021 The subcommandsutil Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package subcommandsutil provides the google/subcomonds utility functions.
+package subcommandsutil
